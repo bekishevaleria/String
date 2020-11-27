48217ad7b7a881b0ab26127f80aceaba0ffeb0db
bekishevaleria <bekisheva.valeria@yandex.ru>
2020-11-27 21:46:56 +0700

    Вывод текста по кнопке

--- app/src/main/java/com/example/string/MainActivity.java
+++ app/src/main/java/com/example/string/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.string;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String str = "Всё, что нам нужно – это";
+    TextView text;
+    Button butt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.text);
+        butt = findViewById(R.id.butt);
+    }
+
+    public void butclick(View view) {
+
+        text.setText(str + Phraser.PhraserGen());
     }
 }
